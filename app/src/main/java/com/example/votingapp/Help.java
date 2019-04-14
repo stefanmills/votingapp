90f01ee9d3d1ea0214e741284a9895bcc6a9115d
stefanmills <stefanmills8@gmail.com>
2019-04-14 19:18:49 +0100

    latest with all buttons

--- app/src/main/java/com/example/votingapp/Help.java
+++ app/src/main/java/com/example/votingapp/Help.java
@@ -1,11 +1,20 @@
 package com.example.votingapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Help extends AppCompatActivity {
+
+    private Button buttonFAQ;
+    private Button buttonContact;
+    private Button buttonTerms;
+    private Button buttonInfo;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,6 +22,20 @@ public class Help extends AppCompatActivity {
         setContentView(R.layout.help);
         ActionBar actionBar= getSupportActionBar();
         actionBar.setTitle("Help");
+
+        buttonInfo = findViewById(R.id.appinfo);
+
+        buttonInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent appinfo= new Intent(getApplicationContext(), AppInfo.class);
+                startActivity(appinfo);
+            }
+        });
+
+
+
 
 
     }
--- app/src/main/java/com/example/votingapp/Settings.java
+++ app/src/main/java/com/example/votingapp/Settings.java
@@ -1,18 +1,59 @@
 package com.example.votingapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
 
 public class Settings extends AppCompatActivity {
+    private Button buttonsetpro;
+    private Button buttonsethelp;
+    private Button buttonsetacc;
+    private Button buttonsetshare;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.settings);
+        buttonsetpro = findViewById(R.id.namebut);
+        buttonsetshare = findViewById(R.id.invite);
+        buttonsetacc = findViewById(R.id.accounts);
+        buttonsethelp = findViewById(R.id.sethelp);
+
+        buttonsetpro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent account= new Intent(getApplication(),Profile.class);
+                startActivity(account);
+
+            }
+        });
+
+        buttonsethelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent help= new Intent(getApplicationContext(),Help.class);
+                startActivity(help);
+            }
+        });
+
+        buttonsetshare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent share= new Intent(getApplicationContext(), Share.class);
+                startActivity(share);
+            }
+        });
+
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Settings");
+
+
+
 
     }
 }
